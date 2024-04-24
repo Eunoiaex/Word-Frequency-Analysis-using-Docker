eeb3e68fcfad8db8492834a011da926fe627e9b5
Tibyan Ashraf <118009175+Eunoiaex@users.noreply.github.com>
2024-04-24 03:04:35 +0200

    Update StopWordsRemoval.py

--- StopWordsRemoval.py
+++ StopWordsRemoval.py
@@ -11,29 +11,17 @@ filtered_words = [word for word in words if word.lower() not in stopwords_list]
 word_counts = Counter(filtered_words)
 for word, count in word_counts.most_common():
     print(f"{word}: {count}")
-# Count occurrences of stop words in filtered text
 stopword_count_filtered = sum(1 for word in filtered_words if word.lower() in stopwords_list)
 
-# Check if stop words are present in the filtered text
 if stopword_count_filtered > 0:
     print("Stop words found in filtered text.")
     print(f"Number of stop words found: {stopword_count_filtered}")
 else:
     print("No stop words found in filtered text.")
-
-    # Specify the file path where you want to save the filtered text
+    
 output_file_path = "filtered_text.txt"
-
-# Open the file in write mode
-    # Write the filtered text to the file
-
 with open('output.txt', 'w') as file:
-    # Convert the list of filtered words to a single string
     filtered_text = ' '.join(filtered_words)
-
-    # Write the filtered text to the file
     file.write(filtered_text)
-
-
 print(f"Filtered text has been written to {output_file_path}")
 
